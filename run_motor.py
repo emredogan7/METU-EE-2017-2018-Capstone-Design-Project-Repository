76a7da96299437c65bcc190ab828fca78edc0471
Göksenin Hande Bayazıt <hande.bayazit@metu.edu.tr>
2017-12-13 21:40:05 +0300

    çift motor

--- run_motor.py
+++ run_motor.py
@@ -6,25 +6,34 @@ mode=GPIO.getmode()
 
 GPIO.cleanup()
 
-Forward=26
-Backward=20
+FwdL=26
+BwdL=20
+FwdR=19
+BwdR=16
 sleeptime=1
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(Forward, GPIO.OUT)
-GPIO.setup(Backward, GPIO.OUT)
+GPIO.setup(FwdL, GPIO.OUT)
+GPIO.setup(FwdR, GPIO.OUT)
+GPIO.setup(BwdL, GPIO.OUT)
+GPIO.setup(BwdR, GPIO.OUT)
 
 def forward(x):
-    GPIO.output(Forward, GPIO.HIGH)
+    GPIO.output(FwdL, GPIO.HIGH)
+    GPIO.output(FwdR, GPIO.HIGH)
     print("Moving Forward")
     time.sleep(x)
-    GPIO.output(Forward, GPIO.LOW)
+    GPIO.output(FwdL, GPIO.LOW)
+    GPIO.output(FwdR, GPIO.LOW)
 
 def reverse(x):
-    GPIO.output(Backward, GPIO.HIGH)
+    GPIO.output(BwdL, GPIO.HIGH)
+    GPIO.output(BwdR, GPIO.HIGH)
     print("Moving Backward")
     time.sleep(x)
-    GPIO.output(Backward, GPIO.LOW)
+    GPIO.output(BwdL, GPIO.LOW)
+    GPIO.output(BwdR, GPIO.LOW)
+
 
 while (1):
     
