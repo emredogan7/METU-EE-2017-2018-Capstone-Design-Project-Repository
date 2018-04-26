86aba02ba9a3ff8a60cd8716253781daa7bdaebf
Burak Sezgin <burak.sezgin@metu.edu.tr>
2018-04-26 16:09:10 +0300

    new_serial_to_be_tested

---
+++ eee_capstone_slave/serial_comm/sensor.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as gpio
+
+# Setup
+import atexit
+import sys
+import time
+import RPi.GPIO as GPIO
+
+mode=GPIO.getmode()
+GPIO.setmode(GPIO.BCM)
+
+TRIG_1 = 21
+ECHO_1 = 20
+
+TRIG_2 = 19
+ECHO_2 = 26
+
+#TRIG_3 = 23
+#ECHO_3 = 24
+
+
+def set_distance_hw(TRIG,ECHO):
+    
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    
+def measure_distance(TRIG,ECHO):
+  GPIO.output(TRIG, False)
+  #print ("Olculuyor...")
+  time.sleep(0.00001)
+
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
+
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
+
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
+
+  pulse_duration = pulse_end - pulse_start
+
+  distance = pulse_duration * 17150
+  distance = round(distance, 2)
+  if TRIG == 19:
+      print ("Front dist :")
+  else:
+      print ("Right dist :")
+  print ( distance )
+  print ("\n")
+  return distance
+    
+
+
+#measure_distance(TRIG_3,ECHO_3)
---
+++ eee_capstone_slave/serial_comm/write.py
@@ -0,0 +1,47 @@
+
+from sensor import set_distance_hw
+from sensor import measure_distance
+import serial
+import time
+ser = serial.Serial('/dev/ttyUSB0',9600)
+mahmut = 9191
+
+Trig_front = 19
+Echo_front = 26
+Trig_right = 21
+Echo_right = 20
+#first integers       = speed
+#second integers = direction (1 for forward, 0 for backward)
+
+set_distance_hw(Trig_front,Echo_front)
+set_distance_hw(Trig_right,Echo_right)
+while ( 1 ) :
+
+    front_dist = measure_distance(Trig_front,Echo_front)
+    right_dist = measure_distance(Trig_right,Echo_right)
+
+    if front_dist <= 8:
+        if right_dist > 20 :
+            mahmut = 6061
+        else:
+            mahmut = 6160
+
+    else:
+        if right_dist <= 4:
+            mahmut = 9181
+        elif right_dist >= 6:
+            mahmut = 9181
+        else :
+            mahmut = 9181
+
+    print ( mahmut )
+
+    ser.write( str(mahmut).encode() );
+    time.sleep(0.01);
+
+    if front_dist <= 8:
+        time.sleep(0.01);
+
+    recieved = ser.read()
+    if int( recieved ) == 170 :
+        print "That's it baby"
