203a9bd0075c0e77c5378666edb60ae55d48b453
Burak Sezgin <burak.sezgin@metu.edu.tr>
2018-04-26 23:45:55 +0300

    al thaa bu da sana aq

--- pwm_1.py
+++ pwm_1.py
@@ -1,28 +1,48 @@
 import RPi.GPIO as GPIO
 
-from RPIO import PWM
+import RPi.GPIO as IO          
 
+import time  
 
-servo = PWM.servo()
 mode = GPIO.getmode()
 GPIO.setmode( GPIO.BCM )
 
-GPIO.setup( ,GPIO.OUT )
-GPIO.setup( ,GPIO.OUT )
+IO.setwarnings(False)          
 
-GPIO.output(,True)
-GPIO.output(,True)
+IO.setmode (IO.BCM)         
 
 
-while True:
-    for i in range (100):
-        servo.set_servo( , 2000 )
-        servo.set_servo( , 2000 )
-    for i in range (100):
-        servo.set_servo( , 1000 )
-        servo.set_servo( , 1000 )
-    for i in range (100):
-        servo.set_servo( , 0 )
-        servo.set_servo( , 0 )
+GPIO.setup(6 ,GPIO.OUT )
+GPIO.setup(5 ,GPIO.OUT )
 
-servo.stop_servo(17)
+GPIO.output(6,True)
+GPIO.output(5,False)
+
+
+GPIO.setup(7 ,GPIO.OUT )
+GPIO.setup(8 ,GPIO.OUT )
+
+GPIO.output(7,False)
+GPIO.output(8,True)
+
+                
+IO.setup(19,IO.OUT)           
+IO.setup(12,IO.OUT)
+
+pwm_0 = IO.PWM(19,100)
+pwm_1 = IO.PWM(12,100)                    
+
+pwm_0.start(0)                              
+pwm_1.start(0)                              
+
+while 1:                               
+
+    for x in range (50):                   
+        pwm_0.ChangeDutyCycle(x)               
+	pwm_1.ChangeDutyCycle(x)               
+        time.sleep(0.1)                     
+      
+    for x in range (50):                          
+        pwm_0.ChangeDutyCycle(50-x)               
+	pwm_1.ChangeDutyCycle(50-x)               
+        time.sleep(0.1)                          
