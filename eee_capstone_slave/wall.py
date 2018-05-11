9941b9a9e250c148eaff3aa3a63e568f74cb76d9
Burak Sezgin <burak.sezgin@metu.edu.tr>
2018-05-11 14:39:19 +0300

    Add files via upload

--- eee_capstone_slave/wall.py
+++ eee_capstone_slave/wall.py
@@ -5,15 +5,17 @@ from numpy import zeros, newaxis
 
 from wall_position_slave import wall_position as m_w_p
 
-cap = cv2.VideoCapture(0)
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
 
 
-ret, frame = cap.read()
-cv2.imshow('Wall_Position',frame)
-while True:
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    ret, frame = cap.read()
+    m_w_p(frame)
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
